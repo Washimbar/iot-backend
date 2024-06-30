8b114f13f06e0e85bd5453a2e93a75e08be7ed03
WashimBar <washimbari0001@gmail.com>
2024-06-30 23:58:13 +0530

    swagger update

--- src/server.js
+++ src/server.js
@@ -1,35 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/users");
-const roomRoutes = require("./routes/rooms");
-const deviceRoutes = require("./routes/devices");
-const connectDB = require("./connection/connectDB");
+const swaggerSetup = require("./swagger");
 
 dotenv.config();
 const PORT = process.env.PORT || 3005;
 
 const app = express();
-
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGO_URI, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
+// Setup Swagger
+swaggerSetup(app);
 
+// Import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const connectDB = require("./connection/connectDB");
+
+// Use routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/rooms", roomRoutes);
-app.use("/api/devices", deviceRoutes);
 
-// app.listen(3000, () => {
-// 	console.log("Server is running on port 3000");
+// Connect to MongoDB
+// mongoose
+// 	.connect(process.env.MONGO_URI, {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(() => console.log("MongoDB connected"))
+// 	.catch((err) => console.log(err));
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+// 	console.log(`Server running on port ${PORT}`);
 // });
 
 const start = async () => {
-	try {;
+	try {
 		await connectDB();
 		app.listen(PORT, () => {
 			console.log(`Server is running at http://localhost:${PORT}`);
--- src/swagger.js
+++ src/swagger.js
@@ -1,26 +1,6 @@
-const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
-
-const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Smart Home Management System API",
-			version: "1.0.0",
-			description: "API documentation for the Smart Home Management System",
-		},
-		servers: [
-			{
-				url: "http://localhost:3000",
-				description: "Development server",
-			},
-		],
-	},
-	apis: ["./routes/*.js"], // Path to the API routes
-};
-
-const specs = swaggerJSDoc(options);
+const swaggerDocument = require("./swagger.json");
 
 module.exports = (app) => {
-	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
