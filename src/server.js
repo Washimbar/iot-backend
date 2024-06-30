ee8f200c275e25baf82ee53478b4c7b78348ac72
WashimBar <washimbari0001@gmail.com>
2024-07-01 00:03:32 +0530

    update

--- src/server.js
+++ src/server.js
@@ -15,25 +15,18 @@ swaggerSetup(app);
 // Import routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
+const roomRoutes = require("./routes/rooms");
+const deviceRoutes = require("./routes/devices");
 const connectDB = require("./connection/connectDB");
 
 // Use routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/rooms", roomRoutes);
+app.use("/api/devices", deviceRoutes);
+
 
 // Connect to MongoDB
-// mongoose
-// 	.connect(process.env.MONGO_URI, {
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 	})
-// 	.then(() => console.log("MongoDB connected"))
-// 	.catch((err) => console.log(err));
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-// 	console.log(`Server running on port ${PORT}`);
-// });
 
 const start = async () => {
 	try {
